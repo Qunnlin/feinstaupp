f39d80932c3fcffa41197190f1d0e9702b588f49
BuildTools <71scfe1bif@hft-stuttgart.de>
2020-01-18 16:29:26 +0100

    added gridding

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -35,149 +35,167 @@ import {MapElementMixin} from 'vue2-google-maps'
                     east: 9.230019239
 
                 };
+ 
 
                 // set Map Options and Zoom
-                map.setOptions({
-                    minZoom: 10,
-                    restriction: {
-                        latLngBounds: mapBounds
-                    }
-                });
+                // map.setOptions({
+                //     minZoom: 10,
+                //     restriction: {
+                //         latLngBounds: mapBounds
+                //     }
+                // });
+                var test = this.getGrid2(mapBounds, 200);
+                test.forEach(cell => {
+                    var rectangle = new google.maps.Rectangle({
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 1,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0,
+                        map: map,
+                        bounds: {
+                            north: cell.north,
+                            south: cell.south,
+                            east: cell.east,
+                            west: cell.west
+                        }
+                    });
+                })
 
                 // add trafficlayer to map
                 let trafficLayer = new google.maps.TrafficLayer;
                 trafficLayer.setMap(map);
 
 
-                // fetch weatherData and add Stations to map
-                let weatherData = this.fetchJSON("http://api.openweathermap.org/data/2.5/find?lat=48.7758459&lon=9.1829321&cnt=15&units=metric&appid=9ed58223d2e011bd45529508e9b9d8b6");
-                weatherData.then( data => {
+                // // fetch weatherData and add Stations to map
+                // let weatherData = this.fetchJSON("http://api.openweathermap.org/data/2.5/find?lat=48.7758459&lon=9.1829321&cnt=15&units=metric&appid=9ed58223d2e011bd45529508e9b9d8b6");
+                // weatherData.then( data => {
 
-                    for(var sensor of data.list){
+                //     for(var sensor of data.list){
 
-                        // create Infobox for station
-                        let weatherConditionInfo = 
-                            '<div id=weatherBox>'+
-                            '<h3>'+sensor.name+'</h3>' +
-                            '<p>Temperatur: ' + sensor.main.temp + ' C°</p>' +
-                            '<p>Gefühlt: ' + sensor.main.feels_like + ' C°</p>' +
-                            '<p>Lufdruck: ' + sensor.main.pressure + ' hPa</p>' +
-                            '<p>Luftfeuchtigkeit: ' + sensor.main.humidity + '%</p>' +
-                            '<b>Windgeschwindigkeit: ' + sensor.wind.speed + ' m/s</b>' +
-                            '</div>';
+                //         // create Infobox for station
+                //         let weatherConditionInfo = 
+                //             '<div id=weatherBox>'+
+                //             '<h3>'+sensor.name+'</h3>' +
+                //             '<p>Temperatur: ' + sensor.main.temp + ' C°</p>' +
+                //             '<p>Gefühlt: ' + sensor.main.feels_like + ' C°</p>' +
+                //             '<p>Lufdruck: ' + sensor.main.pressure + ' hPa</p>' +
+                //             '<p>Luftfeuchtigkeit: ' + sensor.main.humidity + '%</p>' +
+                //             '<b>Windgeschwindigkeit: ' + sensor.wind.speed + ' m/s</b>' +
+                //             '</div>';
 
-                        let infoWindow = new google.maps.InfoWindow({ 
-                            content: weatherConditionInfo});
+                //         let infoWindow = new google.maps.InfoWindow({ 
+                //             content: weatherConditionInfo});
 
-                        // create arrow indicating wind speed & direction
-                        let windArrow = {
-                            path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-                            strokeColor: '#141414'
-                        };
+                //         // create arrow indicating wind speed & direction
+                //         let windArrow = {
+                //             path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+                //             strokeColor: '#141414'
+                //         };
 
-                        // calculate wind direction and draw arrow on map
-                        if(sensor.wind.deg){
-                            let lineFactor = this.calcLengthFactor(sensor.wind.speed);
-                            let endpoint = this.rotation(sensor.coord.lon, sensor.coord.lat+lineFactor, sensor.coord, sensor.wind.deg);
-                            let windLine = new google.maps.Polyline({
-                                path: [{lat: sensor.coord.lat, lng: sensor.coord.lon}, endpoint],
-                                icons: [{
-                                    icon: windArrow,
-                                    offset: '100%'
-                                }],
-                                strokeColor: '#141414',
-                                strokeWeight: 3,
-                                strokeOpacity: 1,
-                                map: map
-                            });
-                            //this.animateWindLine(windLine, lineFactor);
-                        };
+                //         // calculate wind direction and draw arrow on map
+                //         if(sensor.wind.deg){
+                //             let lineFactor = this.calcLengthFactor(sensor.wind.speed);
+                //             let endpoint = this.rotation(sensor.coord.lon, sensor.coord.lat+lineFactor, sensor.coord, sensor.wind.deg);
+                //             let windLine = new google.maps.Polyline({
+                //                 path: [{lat: sensor.coord.lat, lng: sensor.coord.lon}, endpoint],
+                //                 icons: [{
+                //                     icon: windArrow,
+                //                     offset: '100%'
+                //                 }],
+                //                 strokeColor: '#141414',
+                //                 strokeWeight: 3,
+                //                 strokeOpacity: 1,
+                //                 map: map
+                //             });
+                //             //this.animateWindLine(windLine, lineFactor);
+                //         };
 
-                        // draw weather station marker on map
-                        let marker = new google.maps.Marker({
-                            position: {lat: sensor.coord.lat, lng: sensor.coord.lon} ,
-                            icon: "http://openweathermap.org/img/w/" + sensor.weather[0].icon + ".png",
-                            map: map,
-                            title: sensor.name
-                        });
+                //         // draw weather station marker on map
+                //         let marker = new google.maps.Marker({
+                //             position: {lat: sensor.coord.lat, lng: sensor.coord.lon} ,
+                //             icon: "http://openweathermap.org/img/w/" + sensor.weather[0].icon + ".png",
+                //             map: map,
+                //             title: sensor.name
+                //         });
 
-                        // add functionality to station marker
-                        var opened = false;
-                        marker.addListener('click', function() {
-                            if(opened){
-                                infoWindow.close()
-                                opened = false;
-                            } else {
-                                infoWindow.open(map, marker);
-                                opened = true;
+                //         // add functionality to station marker
+                //         var opened = false;
+                //         marker.addListener('click', function() {
+                //             if(opened){
+                //                 infoWindow.close()
+                //                 opened = false;
+                //             } else {
+                //                 infoWindow.open(map, marker);
+                //                 opened = true;
 
-                            }
-                        });
-                    }
-                });
+                //             }
+                //         });
+                //     }
+                // });
 
 
 
-                // fetch current (<5 min) air data
-                var currentSensorData = this.fetchJSON("https://data.sensor.community/airrohr/v1/filter/box="+ mapBounds.north + "," + mapBounds.east + "," + mapBounds.south + "," + mapBounds.west);
-                currentSensorData.then(data => {
+                // // fetch current (<5 min) air data
+                // var currentSensorData = this.fetchJSON("https://data.sensor.community/airrohr/v1/filter/box="+ mapBounds.north + "," + mapBounds.east + "," + mapBounds.south + "," + mapBounds.west);
+                // currentSensorData.then(data => {
 
-                    // sort air data after value_types (temperature, pressure, P1/P2 pollution)
-                    //var sensorIds = this.extractSensorIds(data);
-                    var sortedData = this.sortSensorData(data);
+                //     // sort air data after value_types (temperature, pressure, P1/P2 pollution)
+                //     //var sensorIds = this.extractSensorIds(data);
+                //     var sortedData = this.sortSensorData(data);
 
-                    console.log(sortedData);
-                    sortedData["P1"].forEach(sensor => {
+                //     console.log(sortedData);
+                //     sortedData["P1"].forEach(sensor => {
 
-                        // create marker for each sensor
-                        let marker = new google.maps.Marker({
-                            position:{lat: parseFloat(sensor.location.latitude), lng: parseFloat(sensor.location.longitude)},
-                            map: map,
-                            title: sensor.name
-                        });
+                //         // create marker for each sensor
+                //         let marker = new google.maps.Marker({
+                //             position:{lat: parseFloat(sensor.location.latitude), lng: parseFloat(sensor.location.longitude)},
+                //             map: map,
+                //             title: sensor.name
+                //         });
 
-                        // create pop up window
-                        let sensorInformation = '<div id=sensorBox>'+
-                            '<h3>'+ sensor.sensor.sensor_type.manufacturer + ", " + sensor.sensor.sensor_type.name +'</h3>' +
-                            // '<p>Temperatur: ' + sensor.main.temp + ' C°</p>' +
-                            // '<p>Gefühlt: ' + sensor.main.feels_like + ' C°</p>' +
-                            // '<p>Lufdruck: ' + sensor.main.pressure + ' hPa</p>' +
-                            // '<p>Luftfeuchtigkeit: ' + sensor.main.humidity + '%</p>' +
-                            // '<b>Windgeschwindigkeit: ' + sensor.wind.speed + ' km/h</b>' +
-                            '</div>';
+                //         // create pop up window
+                //         let sensorInformation = '<div id=sensorBox>'+
+                //             '<h3>'+ sensor.sensor.sensor_type.manufacturer + ", " + sensor.sensor.sensor_type.name +'</h3>' +
+                //             // '<p>Temperatur: ' + sensor.main.temp + ' C°</p>' +
+                //             // '<p>Gefühlt: ' + sensor.main.feels_like + ' C°</p>' +
+                //             // '<p>Lufdruck: ' + sensor.main.pressure + ' hPa</p>' +
+                //             // '<p>Luftfeuchtigkeit: ' + sensor.main.humidity + '%</p>' +
+                //             // '<b>Windgeschwindigkeit: ' + sensor.wind.speed + ' km/h</b>' +
+                //             '</div>';
 
-                        let infoWindow = new google.maps.InfoWindow({ 
-                            content: sensorInformation});
+                //         let infoWindow = new google.maps.InfoWindow({ 
+                //             content: sensorInformation});
 
-                        // add functionality
-                        var opened = false;
-                        marker.addListener('click', function() {
-                            if(opened){
-                                infoWindow.close()
-                                opened = false;
-                            } else {
-                                infoWindow.open(map, marker);
-                                opened = true;
+                //         // add functionality
+                //         var opened = false;
+                //         marker.addListener('click', function() {
+                //             if(opened){
+                //                 infoWindow.close()
+                //                 opened = false;
+                //             } else {
+                //                 infoWindow.open(map, marker);
+                //                 opened = true;
 
-                            }
-                        });
+                //             }
+                //         });
 
-                    });
+                //     });
   
-                });
+                // });
                 
-                // add weather overlay from openweathermaps
-                var myMapType = new google.maps.ImageMapType({
-                    getTileUrl: function(coord, zoom) {
-                        return "https://tile.openweathermap.org/map/wind_new/"+ zoom + "/" + coord.x + "/"+ coord.y + ".png?appid=c6a3ae840aa6d20d4b2bb8985779af1e";
-                    },
-                    tileSize: new google.maps.Size(256, 256),
-                    maxZoom: 9,
-                    minZoom: 0,
-                    name: 'mymaptype'
-                });
+                // // add weather overlay from openweathermaps
+                // var myMapType = new google.maps.ImageMapType({
+                //     getTileUrl: function(coord, zoom) {
+                //         return "https://tile.openweathermap.org/map/wind_new/"+ zoom + "/" + coord.x + "/"+ coord.y + ".png?appid=c6a3ae840aa6d20d4b2bb8985779af1e";
+                //     },
+                //     tileSize: new google.maps.Size(256, 256),
+                //     maxZoom: 9,
+                //     minZoom: 0,
+                //     name: 'mymaptype'
+                // });
 
-                //map.overlayMapTypes.insertAt(0, myMapType);
+                // //map.overlayMapTypes.insertAt(0, myMapType);
 
                 
 
@@ -253,6 +271,7 @@ import {MapElementMixin} from 'vue2-google-maps'
 
             },
 
+
             // generate grid over map with fixed cell size (not working correctly)
             getGrid(mapBounds, cellSize){
                 let sw = {lat: mapBounds.south, lng: mapBounds.west};
@@ -261,7 +280,7 @@ import {MapElementMixin} from 'vue2-google-maps'
                 let ne = {lat: mapBounds.north, lng: mapBounds.east};
                 var grid =[];
                 for(var i = 0; i<= this.meterLength(nw, ne); i+=cellSize){
-                    for(var j = 0; j<=this.meterLength(ne, sw); j+=cellSize){
+                    for(var j = 0; j<=this.meterLength(nw, sw); j+=cellSize){
                         let cellBounds = {
                             north: mapBounds.north - this.metersToDeg(j),
                             south: mapBounds.south - this.metersToDeg(j),
@@ -271,6 +290,52 @@ import {MapElementMixin} from 'vue2-google-maps'
                         grid.push(cellBounds);
 
                     }
+                }
+                return grid;
+            },
+
+            correct(p1 , p2) {
+                let distance_meters = meterLength(p1, p2);
+                let distance_deg = metersToDeg(distance_meters);
+                console.log("" + (p1 + distance_deg) + " should be the same as " + p2);
+            },
+
+            // generate grid over map with fixed cell size (not working correctly)
+            getGrid2(mapBounds, cell_count_width){
+                let nw = {lat: mapBounds.north, lng: mapBounds.west};
+                let ne = {lat: mapBounds.north, lng: mapBounds.east};
+                let sw = {lat: mapBounds.south, lng: mapBounds.west};
+                // let se = {lat: mapBounds.south, lng: mapBounds.east};
+                
+                var grid =[];
+
+                console.log("" + ne.lng + "-"  + nw.lng + "=" + (ne.lng - nw.lng))
+                // let cell_width = this.meterLength(nw, ne) / cell_count_width;
+                let cell_width = (ne.lng - nw.lng) / cell_count_width;
+
+                console.log(cell_width);
+                // let cell_count_height = parseInt(this.meterLength(nw, sw) / cell_width);
+                let cell_count_height = parseInt( Math.abs(nw.lat - sw.lat) / cell_width);
+
+                //let cell_height = Math.abs(nw - sw) / cell_count_height;
+                
+
+                for(var i = 0; i<= cell_count_width; i+= 1){
+                    for(var j = 0; j<=cell_count_height; j+=1){
+                        let cellBounds = {
+                            north: mapBounds.north - j*cell_width,
+                            south: mapBounds.north - (j+1)*cell_width,
+                            west: mapBounds.west + i*cell_width,
+                            east: mapBounds.west + (i+1)*cell_width
+                        }
+                        // console.log(this.meterLength({lat: cellBounds.north, lng: cellBounds.west}, {lat: cellBounds.south, lng: cellBounds.east}));
+                        grid.push(cellBounds);
+                    }
+                    // console.log("cell column: " + i);
+                    
+                    // console.log("from " + (mapBounds.west + this.metersToDeg(i*cell_width)) + " to " + (mapBounds.west + this.metersToDeg((i+1)*cell_width)));
+
+
                 }
                 return grid;
             },
